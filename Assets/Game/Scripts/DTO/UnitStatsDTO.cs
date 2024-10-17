158876c428d1cc74af9828bbe016f7c21825b442
tka2704 <tka2704@gmail.com>
2024-10-17 18:36:57 +0300

    base DTOs

---
+++ Assets/Game/Scripts/DTO/UnitStatsDTO.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using FPS.Sheets;
+
+namespace DTO
+{
+	public readonly struct UnitStatsDTO : ISheetDTO
+	{
+		public readonly float Health;
+		public readonly float Armor;
+		public readonly float Range;
+		public readonly float Attack;
+		private readonly string _id;
+
+		public string Id => _id;
+
+		public UnitStatsDTO(IDictionary ht)
+		{
+			SheetsApi.ParseValue(ht[nameof(Id)], out _id);
+			SheetsApi.ParseValue(ht[nameof(Health)], out Health);
+			SheetsApi.ParseValue(ht[nameof(Armor)], out Armor);
+			SheetsApi.ParseValue(ht[nameof(Range)], out Range);
+			SheetsApi.ParseValue(ht[nameof(Attack)], out Attack);
+		}
+	}
+}
---
+++ Assets/Game/Scripts/DTO/UserDTO.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using FPS.Sheets;
+
+namespace DTO
+{
+	public readonly struct UserDTO : ISheetDTO
+	{
+		public readonly Dictionary<string, int> Inventory;
+
+		public string Id => string.Empty;
+
+		public UserDTO(IDictionary ht)
+		{
+			SheetsApi.ParseValue(ht[nameof(Inventory)], out Inventory);
+		}
+	}
+}
